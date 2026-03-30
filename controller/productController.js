5d66306d32b0119e329e483fcac69dab7bf8e804
SULAIMAN <sheikhsulaimantahir@gmail.com>
2026-03-30 13:01:31 +0530

    minor changes

--- controller/productController.js
+++ controller/productController.js
@@ -6,9 +6,11 @@ import { User } from "../model/userModel.js";
 export const getAllProducts = async (req, res) => {
   try {
     const response = await fetch('https://fakestoreapi.com/products');
+    console.log('FakeStore response status:', response.status); // ← add this
     const products = await response.json();
     res.render('products', { products, user: req.user });
   } catch (error) {
+    console.log('FakeStore fetch error:', error.message); // ← add this
     res.status(500).send('Failed to fetch products');
   }
 };
