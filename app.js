752139f3b5d8476d71fe9805df9591293deb607f
SULAIMAN <sheikhsulaimantahir@gmail.com>
2026-04-02 05:41:18 +0530

    small changes

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
@@ -19,7 +18,6 @@ mongoose.connect(process.env.MONGO_URL)
 app.set('view engine', 'ejs');
 
 
-app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
